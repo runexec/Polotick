b3c2b26c090bff533319a44cca2532a52d50d15d
Ryan Kelker <theinternet@yandex.com>
2016-10-06 06:42:13 +0900

    Add ticker selection via env vars

--- ticker.js
+++ ticker.js
@@ -3,7 +3,7 @@
 var config = [];
 
 // Poloniex Config
-config['PAIR_KEY'] = 'BTC_NAV'
+config['PAIR_KEY'] = process.env.polopair || 'BTC_NAV'
 
 // Browser Config
 config['USER_AGENT'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0'
